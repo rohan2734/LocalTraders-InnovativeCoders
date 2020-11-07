5c4611f10f885da9a874591f4f6b8bb14fa479a3
rohan2734 <jagandevaki1@gmail.com>
2020-11-07 22:54:05 +0530

    connected to the database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose');
 const app = express();
+
+//bodyParser
+app.use(bodyParser.json())
 
 //import keys
 const keys = require('./keys/keys');
@@ -11,9 +14,21 @@ const investmentRouters = require('./routes/investor-routes');
 
 
 //registering routes as middlewares
-app.use(investmentRouters);
+app.use('/api/investors',investmentRouters);
 
-
-app.listen(keys.PORT,() => {
-    console.log(`listening on ${keys.PORT}`);
+mongoose
+.connect(
+    `mongodb+srv://${keys.mongoDBUsername}:${keys.mongoDBPassword}@cluster0.3kcv6.mongodb.net/${keys.DBName}?retryWrites=true&w=majority`,
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+.then(()=>{
+    app.listen(keys.PORT,() => {
+        console.log(`listening on ${keys.PORT}`);
+    })
 })
+.catch(err => {
+    console.log(err)
+})
+
